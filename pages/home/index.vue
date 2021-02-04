9493885a6af181b1b852f9fa4bf4b5375c21f4c6
杨焱康 <704799639@qq.com>
2021-02-04 12:29:13 +0800

    test deploy

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -4,7 +4,7 @@
     <div class="banner">
       <div class="container">
         <h1 class="logo-font">conduit</h1>
-        <p>One Day Tommy-Yang.</p>
+        <p>One Day  Tommy-Yang.</p>
       </div>
     </div>
 
